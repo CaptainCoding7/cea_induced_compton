c91d49b5d5397cebb3de7c2e98ef4bc0ea2a49ae
CaptainCoding7 <46071575+CaptainCoding7@users.noreply.github.com>
2021-06-11 16:46:50 +0200

    V2.3
    - Plots de la densité de photons (spectrale et énergétique) et de l'intensité
    - Ajout des courbes théoriques

--- kompaneets_cc2.py
+++ kompaneets_cc2.py
@@ -11,6 +11,8 @@ import numpy as np
 from scipy.stats import norm
 from scipy.signal import unit_impulse
 from scipy.stats import lognorm
+from scipy.integrate import nquad
+from scipy.integrate import quad
 from math import pow
 from math import exp
 from math import sinh
@@ -20,6 +22,10 @@ from plotter import logplot
 from scipy.special import iv as Inu
 import matplotlib.pyplot as plt
 from matplotlib import cm
+from sympy import symbols, Eq, solve
+from scipy.optimize import fsolve
+
+
 
 """
 x: array of the energy x=(h.nu)/(k.T) with nu the frequency
@@ -71,7 +77,7 @@ def setFigureParameters(ylabel, xlabel, ymin, ymax,  xmin, xmax):
     # Reglages affichage
     plt.xscale('log')
     plt.yscale('log')
-    leg = plt.legend(loc="upper right",prop={'size': 8}, bbox_to_anchor=[1, 1],
+    leg = plt.legend(loc="upper right",prop={'size': 7}, bbox_to_anchor=[1, 1],
                      ncol=1, shadow=True, title="Legend", fancybox=True)
     leg.get_title().set_color("black")
     plt.ylim(ymin,ymax)
@@ -131,40 +137,14 @@ def plotOccupationRate():
     setFigureParameters('u(x,t)','x')
     
     
-def plotDensity():
-                  
-    phoDensTh =  (1 / (np.exp((h*f_photons)/(k*Te)) - 1 )) * e_photons**2
-    #plt.plot(e_photons, phoDensTh, ":",label='theoritical solution')
-    
-    # Calculus of the final density (after 30s)
-    # Must be constant (same as the initial one)
-    Ndens = np.sum(u[N-1]*xa*xa*dxa)
-    #print("Densité finale: ",Ndens)
-
-    for i,tt in enumerate(tobs):
-        print("Tracé ",i+1)
-        index = int(tt / dt) - 1
-        if index==-1:
-            index=0
-        print(index)
-        
-        # plot the photon density rate
-        #phoDens = ((8*np.pi)/cl**3) * (u[index]*f_photons**3) / e_photons
-        phoDens = u[index]*e_photons**2
-        # multiply by Enorm as we represent the energy in abscisse
-        plt.plot(e_photons, phoDens, color = col[i],label='t={:.2f}s '.format(tt))
-    
-    setFigureParameters('Photon Number Density $ (cm^{-3} . keV^{-1})$','Energy (keV)',1e-6,1e2,1e-4,1e4)
-
 def plotDensityFreq():
                   
-    phoDensTh = ((8*np.pi)/cl**3) * ( (1 / (np.exp((h*f_photons)/(k*Te)) - 1 ))   *f_photons**2)
-    #plt.plot(f_photons, phoDensTh, ":",label='theoritical solution')
-
+    phoDensTh = ((8*np.pi)/cl**3) * f_photons**2 / (findCst()*np.exp((h*f_photons)/(k*Te)) - 1 ) 
+    plt.plot(f_photons, phoDensTh, "+",color='red',label='theoritical solution')
     
     # Calculus of the final density (after 30s)
     # Must be constant (same as the initial one)
-    Ndens = np.sum(u[N-1]*xa*xa*dxa)
+    Ndens0 = 8*np.pi * ((k*Te)/(cl*h))**3 * np.sum(u[N-1]*dxa*xa**2)
     #print("Densité finale: ",Ndens)
 
     for i,tt in enumerate(tobs):
@@ -179,13 +159,50 @@ def plotDensityFreq():
         # multiply by Enorm as we represent the energy in abscisse
         plt.plot(f_photons, phoDens, color = col[i],label='t={:.2f}s '.format(tt))
     
-    setFigureParameters('Spectral Density $ (cm^{-3} . Hz^{-1})$','Frequency (Hz)',1e2,1e15,1e12,1e21)
+    setFigureParameters('Spectral Density $ (nbPhotons . m^{-3} . Hz^{-1})$','Frequency (Hz)',1e2,1e13,1e13,1e21)
+    
+    
+def plotDensity():
+                 
+    phoDensTh = ((8*np.pi)/cl**3) * f_photons**2 * (1 / (findCst()*np.exp((h*f_photons)/(k*Te)) - 1 )) *1.602e-16 / h 
+    plt.plot(e_photons, phoDensTh, "+",color='red',label='theoritical solution')
+
+    # Calculus of the final density (after 30s)
+    # Must be constant (same as the initial one)
+    Ndens = np.sum(u[N-1]*xa*xa*dxa)
+    #print("Densité finale: ",Ndens)
+
+    for i,tt in enumerate(tobs):
+        print("Tracé ",i+1)
+        index = int(tt / dt) - 1
+        if index==-1:
+            index=0
+        print(index)
+        
+        # plot the photon density rate
+        """
+        two to convert the density from nbPho/m3/Hz into nbPho/m3/kev
+        - 1kev = 2.4e17
+        - E=h.nu ==> divide by h (and  multiply by 1.602e-16)
+        """
+        #phoDens = ((8*np.pi)/cl**3) * (u[index]*(f_photons**2)) * 2.4e17
+        phoDens = ((8*np.pi)/cl**3) * (u[index]*(f_photons**2)*1.602e-16) / h 
+        #phoDens = u[index]*e_photons**2
+        # multiply by Enorm as we represent the energy in abscisse
+        plt.plot(e_photons, phoDens, color = col[i],label='t={:.1E}s'.format(tt))
+    
+    setFigureParameters('Photon Number Density $ (nbPhotons . m^{-3} . keV^{-1})$','Energy (keV)',1e20,1e30,1e-4,1e3)
+
 
 def plotIntensity():
- 
-    intensityTh = ((2*h)/cl**2)  * ( (1 / (np.exp((h*f_photons)/(k*Te)) - 1 ))   *f_photons**3)
-    #plt.plot(e_photons, intensityTh, ":",label='theoritical solution')
-
+    
+    intensityTh = ((2*h)/cl**2)  * ( (1 /  (findCst()*np.exp((h*f_photons)/(k*Te)) - 1 )) * f_photons**3)
+    plt.plot(e_photons, intensityTh, "+", color='red',label='theoritical solution')
+    
+    # Calculus of the final density (after 30s)
+    # Must be constant (same as the initial one)
+    Ndens = 8*np.pi * ((k*Te)/(cl*h)) * np.sum(u[N-1]*xa*xa*dxa)
+    print("Densité finale: ",Ndens)
 
     for i,tt in enumerate(tobs):
         print("Tracé ",i+1)
@@ -198,10 +215,9 @@ def plotIntensity():
         intensity = ((2*h)/cl**2) * (u[index]*f_photons**3)
 
         # multiply by Enorm as we represent the energy in abscisse
-        
-        plt.plot(e_photons, intensity, color = col[i],label='t={:.2f}s '.format(tt))
+        plt.plot(e_photons, intensity, color = col[i],label='t={:.1E}s'.format(tt))
     
-    setFigureParameters('Spectral Radiance','Energy (keV)',1e-2,1e4,1e-2,1e3)
+    setFigureParameters('Spectral Radiance $(keV.m^{-2}.s^{-1}.Hz^{-1}.str^{-1})$','Energy (keV)',1e-2,1e4,1e-1,1e3)
 
 
 def plotEnergyDensity():
@@ -222,14 +238,40 @@ def plotEnergyDensity():
     plt.plot(t, Ee,label='Ee')    
     plt.plot(t, Er,label='Er')
         
-    setFigureParameters('Photon Energy Density $ (erg . cm^{-3})$','Time (s)',1e12, 1e21,0,tmax/5)
+    setFigureParameters('Photon Energy Density $ (erg . m^{-3})$','Time (s)',1e12, 1e21,0,tmax/5)
     
+    
+def BEDistrib(x,c):
+    """
+    return the Bose-Einstein solution from x and c
+    """
+    return x**2 / (c*np.exp(x)-1)
+    
+
+def F(c):
+    
+    """
+    the equation used to find the constant
+    """
+    Ndens0 = 8*np.pi * ((k*Te)/(cl*h))**3 * np.sum(u[0]*dxa*xa**2)
+    print("Densité initiale: ",Ndens0)
+    eq = quad(BEDistrib, 0, 100, args=(c,)) - ((cl*h)**3*Ndens0) / (8*np.pi*(k*Te)**3)
+    return eq[0]
+
+
+def findCst():
+    """
+    Function used to find the cst of the Bose-Einstein solution
+    """    
+    root = fsolve(F,2)
+    print("cst = ",root[0])
+    return root[0]
 
 ####################################################
 
-def main():
-
-    global xb,xa,dxa,dxb,k,h,Te,cl,Ne,tobs,tmax,dt,dto,N,M,u,A,C,Enorm,col, f_photons, e_photons
+def defineConstants():
+    
+    global k,h,Te,cl,sT,Ne,me,tobs
 
     #################  CONSTANTS  #################
     
@@ -244,32 +286,34 @@ def main():
     # Thompson scattering cross section
     sT = 6.652e-29
     # Electron number density
-    Ne=6e22
+    Ne=6e28
     # Electron mass
     me=9.109e-31
     
-    ##############  MESH GENERATION  ###############
+
+def meshGeneration():
     
-    #### time mesh
+    global xb,xa,dxa,dxb,tobs,tmax,dt,dto,N,M,u,Enorm, f_photons, e_photons
+
+    #########################  MESH GENERATION  #######################
+    
+    #### time mesh######################################################
+    
+    nu = ((k*Te) / (me*cl**2)) * Ne * cl * sT
     
     # observation instants
     # the last instant is takenn as the max time in the simulation
-    #tobs = [0.0,0.05,0.1,0.15,0.2,3]
-    #tobs = [0,0.1,0.15,0.2,0.25,0.3,0.35,0.4,10]
-    #tobs = [0,0.005,0.01,0.02,0.03,0.04,0.05,0.06,0.07]
-    #tobs = [0.07,0.1,0.13,0.16,0.19]
-    #tobs = [1,2,3,4,5,6,7,8,9]
-    #tobs = [0,5e-9,5e-8,1e-7]
-    tobs = [0,10]
+    #tobs = 5e-9 * np.arange(12)
+    #tobs = np.arange(0,26e-8,5e-8)
+    tobs = [0, 5e-6]
     tmax= tobs[len(tobs)-1]
     
-    dt=5e-3
-    dto = ((k*Te) / (me*cl**2)) * Ne * cl * sT * dt
-    
+    dt=5e-9
+    dto =  nu * dt
     print("dt;dto: ",dt,dto)
     N=int(tmax/dt)
     
-    #### energy mesh
+    #### energy mesh  ####################################################
     
     M=1000
     
@@ -302,33 +346,33 @@ def main():
     # distance entre les milieux (M-1 valeurs)
     dxb = xa[1:] - xa[:-1]
     
+    # energy and frequency vectors
     e_photons = xa * Enorm
     # *1.602e-16 keV ==> J
     f_photons = e_photons * 1.602e-16 / h
     
-
-    #################  MAIN & PARAMETERS  #################
-    
-    # initialization of the solution vector
+    ### initialization of the solution vector : [time][energy] (x is a pseudo-energy)
     
     u=np.empty([N,M])
     # photons injection at 0.5 keV, gaussian width of 0.03keV
     xinj = 0.5 / Enorm
     width = 0.03 / Enorm
     print(xinj, width)
-    u[0] = init_gaussian(xinj, width)
+    u[0]=init_gaussian(xinj, width)
     
     # Cette fois, on impose le nombre de photons
     # STABLE EN DECA DE ~2.4
     #nbPhotons = 1
     
-    # Calculus of the initial density
-    # simple integration
-    Ndens = np.sum(u[0]*xa*xa*dxa)
-    #print("Densité initiale: ",Ndens)
+def solveKompaneets():
     
-    # Initialization of the A, B, C, Q, w and W vectors
+    global A,C,Q,pech,col
+    
+    ################# KOMPANEETS EQUATION  PARAMETERS  #################
+    
+    # Initialization of the A, C, Q
     # the vectors containing the terms from the Kompaneets equation
+    # B is defined in the changCooper function as it evolves with the time
     
     A = xa*xa 
     C =  0.5*(xa[1:]**4+xa[:-1]**4) # sur les bords      (M-1)
@@ -351,15 +395,19 @@ def main():
     # Plotting parameter
     col = [ cm.jet(x) for x in np.linspace(0, 0.3 , len(tobs))]
     
-    #plotDensityFreq()
-    
+    plotDensityFreq()
     #plotDensity()
-    
     #plotOccupationRate()
-    
-    plotIntensity()
-    
+    #plotIntensity()
     #plotEnergyDensity()
+    
+
+def main():
+    
+    defineConstants()
+    meshGeneration()
+    solveKompaneets()
+    
 
 
 if __name__ == "__main__":
