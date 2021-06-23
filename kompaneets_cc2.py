79a1347df87d9f6e2f2c5c69ab21562a7ed76a3d
CaptainCoding7 <46071575+CaptainCoding7@users.noreply.github.com>
2021-06-23 17:22:57 +0200

    v5.0 - kompaneets_cc2.py
    - Déplacement des fonctions annexes dans d'autres fichiers (plotter.py, thSol.py...)

--- kompaneets_cc2.py
+++ kompaneets_cc2.py
@@ -8,21 +8,16 @@ Created on Thu Jun  3 13:44:28 2021
 # Script to find a solution for the equation of Kompaneets with the Chang-Cooper scheme
 
 import numpy as np
-from scipy.stats import norm
-from scipy.signal import unit_impulse
-from scipy.stats import lognorm
-from scipy.integrate import quad
-from scipy.special import zeta
-from scipy.special import gamma as Gammaf
-import tridiagonal_matrix_solver as tms
+
+from constants import k,h,Te,cl,sT,Ne,me,qe,mu0,kTe, Tp, kTp
+import changcooper as cc
 from plotter import setFigureParameters
 from plotter import noLogsetFigureParameters
-import matplotlib.pyplot as plt
-from matplotlib import cm
-from scipy.optimize import fsolve
-from synchrotron import synch
-from constants import k,h,Te,cl,sT,Ne,me,qe,mu0,tobs, kTe, Tp, kTp
-
+from plotter import plotIntensity
+from thSol import findCst
+from thSol import findNmax
+from initDistrib import init_gaussian
+from initDistrib import init_planckian
 
 """
 x: array of the energy x=(h.nu)/(k.T) with nu the frequency
@@ -31,248 +26,13 @@ u: solutions of the equation for different instants
 """
 
 
-def init_gaussian(xinj, width):
-    """
-    initialization of the photons distribution with a gaussian profile
-    """
-
-    # on identifie le bin correspondant a xinj
-    iinj = np.argwhere(xb<xinj)[-1]
-    # on recentre xinj sur le bin en question
-    xinj = xa[iinj]
-
-    u0 = norm.pdf(xa , loc = xinj , scale = width )
-
-    return u0
-
-
-def init_dirac(xinj):
-    """
-    initialization of the photons distribution with a dirac profile
-    """
-        
-    #xinj = np.sqrt(nbPhotons)
-    
-    # on identifie le bin correspondant a xinj
-    iinj = np.argwhere(xb<xinj)[-1]
-    # on recentre xinj sur le bin en question
-    xinj = xa[iinj]
-    # on initialise avec des zeros partout sauf dans le bin en question
-    # ou on met 1/dx de maniere avec avoir une integrale=1
-    #u0 = np.zeros_like(xa)
-    #u0[iinj] = 1.0/dxa[iinj]
-
-    nbPhotons = xinj**2
-
-    uinj = nbPhotons / ((xinj**2) * dxa[iinj])
-    
-    u0 = uinj * unit_impulse(len(xa), int(iinj))
-
-    return u0
-
-def init_planckian(Tp):
-    """
-    initialization of the photons distribution with a planckian profile
-    """
-    
-    #u0 = (1 /  (np.exp((h*f_photons)/(k*Tp)) - 1 ))
-    u0 = (1 /  (np.exp(e_photons/kTp) - 1 ))
-    return u0
-
-
-
-def changCooper(pech, Q, dT):
-    """
-    solve the equation with the chang-cooper scheme
-    """
-    
-    #print(dT)
-    
-    #dT=dt
-
-    for n in range(N-1):
-        
-       #we redefine B as it depends on u    
-        B = 0.5*(xa[1:]**4+xa[:-1]**4) * (u[n][1:]+1) # sur les bords      (M-1)
-        w = (B/C)*dxb
-        lwl = np.abs(w)
-        W = lwl * np.exp(-lwl)/(1.0-np.exp(-lwl))
-        Wp = W*np.exp(+w/2)
-        Wm = W*np.exp(-w/2)
-        
-        
-        # sous-diagonale (taille M-1)
-        a = 1/A[1:]/dxa[1:] * C/dxb * Wm
-    
-        # sur-diagonale (taille M-1)
-        c = 1/A[:-1]/dxa[:-1] * C/dxb * Wp
-    
-        # diagonale (taille M)
-        b = np.zeros_like(xa)
-        b[0]    = 1/dT + 1/A[   0]/dxa[   0] * (                         0 + C[0 ]/dxb[ 0]*Wm[ 0] ) + pech[0]
-        b[1:-1] = 1/dT + 1/A[1:-1]/dxa[1:-1] * ( C[:-1]/dxb[:-1] * Wp[:-1] + C[1:]/dxb[1:]*Wm[1:] ) + pech[1:-1]
-        b[-1]   = 1/dT + 1/A[  -1]/dxa[  -1] * ( C[ -1]/dxb[ -1] * Wp[ -1] +                    0 ) + pech[-1]
-        
-        r = Q + u[n]/dT
-                
-        u[n+1] = tms.tridag(-a,b,-c,r)
-        
-        #print("---- u = ",u)
-            
-    return u
-
-
-def plotOccupationRate():
-    
-    print("Plotting occupation rate...")
-        
-    # plot the occupation rate
-    for i,tt in enumerate(tobs):
-        print("Tracé ",i+1)
-        index = int(tt / dt) - 1
-        plt.plot(xa,u[index],color=col[i],label='t={:.2f}s '.format(index*tmax/5))    
-    
-    setFigureParameters('u(x,t)','x')
-    
-    
-def plotDensityFreq():
-    """
-    plot the evolving photons density as a function of the frequency
-    """
-    print("Plotting spectral density...")
-                  
-    phoDensTh = ((8*np.pi)/cl**3) * f_photons**2 / (findCst()*np.exp((h*f_photons)/(k*Te)) - 1 ) 
-    plt.plot(f_photons, phoDensTh, "+",color='red',label='theoritical solution')
-    
-    # Calculus of the final density (after 30s)
-    # Must be constant (same as the initial one)
-    Ndens0 = 8*np.pi * ((k*Te)/(cl*h))**3 * np.sum(u[N-1]*dxa*xa**2)
-    #print("Densité finale: ",Ndens)
-
-    for i,tt in enumerate(tobs):
-        print("Tracé ",i+1)
-        index = int(tt / dt) - 1
-        if index==-1:
-            index=0
-        
-        # plot the photon density rate
-        phoDens = ((8*np.pi)/cl**3) * (u[index]*f_photons**2)
-
-        # multiply by Enorm as we represent the energy in abscisse
-        plt.plot(f_photons, phoDens, color = col[i],label='t={:.2f}s '.format(tt))
-    
-    setFigureParameters('Spectral Density $ (nbPhotons . m^{-3} . Hz^{-1})$','Frequency (Hz)',1e2,1e13,1e13,1e21)
-    
-    
-def plotDensity():
-    """
-    plot the evolving photons density as a function of the energy (keV)
-    """
-    print("Plotting density...")
-                 
-    phoDensTh = ((8*np.pi)/cl**3) * f_photons**2 * (1 / (findCst()*np.exp((h*f_photons)/(k*Te)) - 1 )) *1.602e-16 / h 
-    plt.plot(e_photons, phoDensTh, "+",color='red',label='theoritical solution')
-
-    # Calculus of the final density (after 30s)
-    # Must be constant (same as the initial one)
-    Ndens = np.sum(u[N-1]*xa*xa*dxa)
-    #print("Densité finale: ",Ndens)
-
-    for i,tt in enumerate(tobs):
-        print("Tracé ",i+1)
-        index = int(tt / dt) - 1
-        if index==-1:
-            index=0
-        print(index)
-        
-        # plot the photon density rate
-        """
-        two ways to convert the density from nbPho/m3/Hz into nbPho/m3/kev
-        - 1kev = 2.4e17 Hz
-        - E=h.nu ==> divide by h (and  multiply by 1.602e-16)
-        """
-        #phoDens = ((8*np.pi)/cl**3) * (u[index]*(f_photons**2)) * 2.4e17
-        phoDens = ((8*np.pi)/cl**3) * (u[index]*(f_photons**2)*1.602e-16) / h 
-        plt.plot(e_photons, phoDens, color = col[i],label='t={:.1E}s'.format(tt))
-    
-    setFigureParameters('Photon Number Density $ (nbPhotons . m^{-3} . keV^{-1})$','Energy (keV)',1e20,1e30,1e-4,1e3)
-
-
-def plotIntensity():
-    """
-    plot the sepctral radiance (intensité specifique) as a function of the energy
-    """
-    
-    print("Plotting intensity...")
-    
-    """
-    comparison with the theoritical solution
-    """
-    #C=findCst()
-    #intensityTh = ((2*h)/cl**2) * f_photons**3 * ( (1 /  (C*np.exp((h*f_photons)/(k*Te)) - 1 ))) 
-    #plt.plot(e_photons, intensityTh, "+", color='red',label='theoritical solution')
-    
-    # Calculus of the final density (after 30s)
-    # Must be constant (same as the initial one)
-    Ndens = 8*np.pi * ((k*Te)/(cl*h)) * np.sum(u[N-1]*xa**2*dxa)
-    print("Densité finale: ",Ndens)
-
-    for i,tt in enumerate(tobs):
-        print("Tracé ",i+1)
-        index = int(tt / dt) - 1
-        if index==-1:
-            index=0
-        print(index)
-
-        # plot the intensity of the photon field
-        intensity = ((2*h)/cl**2) * (u[index]*f_photons**3)
-        
-        # multiply by Enorm as we represent the energy in abscisse
-        plt.plot(e_photons, intensity, color = col[i],label='t={:.1E}s'.format(tt))
-    
-
-    setFigureParameters('Spectral Radiance $(keV.m^{-2}.s^{-1}.Hz^{-1}.str^{-1})$','Energy (keV)',1e-1,5e7,1e-4,1e3)
-
-    
-def BEDistrib(x,c):
-    """
-    return the Bose-Einstein solution from x and c
-    """
-    return x**2 / (c*np.exp(x)-1)
-    
-
-def F(c):
-    """
-    the equation used to find the constant
-    """
-    eq = quad(BEDistrib, 0, 100, args=(c,)) - ((cl*h)**3*Ndens0) / (8*np.pi*(k*Te)**3)
-    return eq[0]
-
-
-def findCst():
-    """
-    Function used to find the cst of the theoritical Bose-Einstein solution
-    """    
-    root = fsolve(F,2)
-    print("cst = ",root[0])
-    return root[0]
-
-####################################################
-
-def findNmax():
-    """
-    the theoritical maximal density below which there is no BE condensate
-    """
-    Nmax = 8*np.pi*(((k*Te)/(cl*h))**3)*2*zeta(3)
-    return Nmax
-
 
 def meshGeneration():
     """
     generation of the time and energy meshes
     """
     
-    global xb,xa,dxa,dxb,tobs,tmax,dt,dto,N,M, f_photons, e_photons
+    global N, M, f_photons, e_photons
 
     #########################  MESH GENERATION  #######################
     
@@ -284,8 +44,8 @@ def meshGeneration():
     # the last instant is taken as the max time in the simulation
     #tobs = 5e-9 * np.arange(12)
     #tobs = np.arange(0,26e-8,5e-8)
-    tobs = [0, 1e-7, 3e-7, 4e-7]
-    tobs = [0, 2e1] # stabilité atteinte    
+    tobs = [1e-7, 3e-7, 4e-7]
+    tobs = [2e1] # stabilité atteinte    
     
     tmax= tobs[len(tobs)-1]
     
@@ -323,14 +83,14 @@ def meshGeneration():
     # distance entre les milieux (M-1 valeurs)
     dxb = xa[1:] - xa[:-1]
     
+    return xa,xb,dxa,dxb,tobs,tmax,dt,dto
     
-def solveKompaneets():
+    
+def solveKompaneets(xa,xb,dxa,dxb,tobs,tmax,dt,dto):
     """
     solve the Kompaneets equation with the chang-cooper scheme
     call on of the plotting function
     """
-    
-    global A,C,Q,pech,col, u, Ndens0
     
     ################# KOMPANEETS EQUATION  PARAMETERS  #################
     
@@ -343,7 +103,6 @@ def solveKompaneets():
     
     # injection term
     Q = np.full_like(xa, 0)            # au milieu          (M)
-    #Q=init_cond(xinj)
     
     # sink term
     #n*ro = n/T(x) => T(x) = 1/ro
@@ -352,44 +111,60 @@ def solveKompaneets():
     
     ### initialization of the solution vector : [time]["energy"] (x is a pseudo-energy)
     
-    u=np.empty([N,M])
+    #u=np.empty([N,M])
+    
     # photons injection at 0.5 keV, gaussian width of 0.03keV
     einj = 0.5
     ewidth = 0.03
     xinj = einj / kTe
     width = ewidth / kTe
     print(xinj, width)
-    #u[0]=init_gaussian(xinj, width)
-    u[0] = init_planckian(Tp)
+    u0 = init_planckian(e_photons)
     
     # Cette fois, on impose le nombre de photons
     # STABLE EN DECA DE ~2.4
     #nbPhotons = 1
     
-    Ndens0 = 8*np.pi * ((k*Te)/(cl*h))**3 * np.sum(u[0]*dxa*xa**2)
+    Ndens0 = 8*np.pi * ((k*Te)/(cl*h))**3 * np.sum(u0*dxa*xa**2)
     print("Densité initiale: ",Ndens0)
     
     
     ############  SOLVING KOMPANEETS EQUATION AND PLOTTING FUNCTIONS  ###########
+        
+    # find the solution of the kompaneets equation with the cc scheme for each instant
+    # specified in tobs
+    uobs = np.empty([len(tobs)+1,M])
+    uobs[0] = u0
+    print(len(uobs[0]))
+    for i,tt in enumerate(tobs):    
+        N=int(tmax/dt)
+        uobs[i+1] = cc.changCooper(A, C, xa, dxa, dxb, N, u0, pech, Q, dto)
     
-    # update the vector u with the solution of the Kompaneets equation for each instant
-    changCooper(pech,Q,dto)
+    # Calculus of the final density (after 30s)
+    # Must be constant (same as the initial one)
+    umax=uobs[len(uobs)-1]
+    Ndens = 8*np.pi * ((k*Te)/(cl*h)) * np.sum(umax*xa**2*dxa)
+    print("Densité finale: ",Ndens)
     
-    # Plotting parameter
-    col = [ cm.jet(x) for x in np.linspace(0, 0.3 , len(tobs))]
+    C=findCst(Ndens0)
+    intensityTh = ((2*h)/cl**2) * f_photons**3 * ( (1 /  (C*np.exp((h*f_photons)/(k*Te)) - 1 ))) 
     
-    #plotDensityFreq()
-    #plotDensity()
-    #plotOccupationRate()
-    plotIntensity()
-    #plotEnergyDensity()
+    plotIntensity(Ndens0, uobs, tobs, e_photons, f_photons)
+    
+    phoDensTh = ((8*np.pi)/cl**3) * f_photons**2 / (findCst(Ndens0)*np.exp((h*f_photons)/(k*Te)) - 1 ) 
+    #plotDensityFreq(xa, dxa, tobs, dt)
+    
+    #phoDensTh = ((8*np.pi)/cl**3) * f_photons**2 * (1 / (findCst(Ndens0)*np.exp((h*f_photons)/(k*Te)) - 1 )) *1.602e-16 / h 
+    #plotDensity(xa, dxa, tobs, dt)
+    
+    #plotOccupationRate(xa, dxa, tobs, dt)
 
 
 def mainKompaneets():
     
-    meshGeneration()
+    xa,xb,dxa,dxb,tobs,tmax,dt,dto = meshGeneration()
 
-    solveKompaneets()  
+    solveKompaneets(xa,xb,dxa,dxb,tobs,tmax,dt,dto)  
     
     Nmax=findNmax()
     
