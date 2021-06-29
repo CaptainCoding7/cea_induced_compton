b4689a9db89595687a21a6a86939cae53ccdaed1
CaptainCoding7 <46071575+CaptainCoding7@users.noreply.github.com>
2021-06-29 14:07:19 +0200

    v6.0 kompaneets_cc2.py
    - Résolution de kompaneets avec synchrotron
    - Tracé des composantes synchrotron et compton superposées au spectre total en intensité (/!\ tracé des composantes incorrect et a revoir)

--- kompaneets_cc2.py
+++ kompaneets_cc2.py
@@ -8,16 +8,20 @@ Created on Thu Jun  3 13:44:28 2021
 # Script to find a solution for the equation of Kompaneets with the Chang-Cooper scheme
 
 import numpy as np
+import matplotlib.pyplot as plt
 
-from constants import k,h,Te,cl,sT,Ne,me,qe,mu0,kTe, Tp, kTp
+from constants import k,h,Te,cl,sT,Ne,me,kTe, B, Bcgs, R, Rcgs
 import changcooper as cc
-from plotter import setFigureParameters
-from plotter import noLogsetFigureParameters
 from plotter import plotIntensity
+from plotter import plotIntensityAll
+from plotter import setFigureParameters
 from thSol import findCst
 from thSol import findNmax
 from initDistrib import init_gaussian
 from initDistrib import init_planckian
+from synchrotron import J_nu_theta
+from synchrotron import B_nu
+from synchrotron import setParameters
 
 """
 x: array of the energy x=(h.nu)/(k.T) with nu the frequency
@@ -25,43 +29,35 @@ u: solutions of the equation for different instants
 
 """
 
-
-
 def meshGeneration():
     """
     generation of the time and energy meshes
     """
     
-    global N, M, f_photons, e_photons
-
     #########################  MESH GENERATION  #######################
     
     #### time mesh  ################################
     
-    nu = ((k*Te) / (me*cl**2)) * Ne * cl * sT
+    # pseudo-time
+    #to = ((k*Te) / (me*cl**2)) * Ne * cl * sT
+    to = cl / R
     
     # observation instants
-    # the last instant is taken as the max time in the simulation
-    #tobs = 5e-9 * np.arange(12)
-    #tobs = np.arange(0,26e-8,5e-8)
-    tobs = [1e-7, 3e-7, 4e-7]
-    tobs = [2e1] # stabilité atteinte    
-    
-    tmax= tobs[len(tobs)-1]
-    
-    dt=1e-2
-    dto =  nu * dt
+    # the last instant is taken as the max time in the simulation   
+    tobs = [1.e-3, 1.e-2, 5e-2,1e-1]
+    tobs = [1.e-1]
+    dt=1e-5
+    dto =  to * dt
     #print("dt;dto: ",dt,dto)
-    N=int(tmax/dt)
     
     #### "energy" mesh  ################################
 
-    M=1000
+    M=100
     
     # the energy carried by the photons (keV)
-    emin = 1e-3
-    emax = 1e3
-    e_photons = np.linspace(emin,emax,M)   
+    emin = 5e-9
+    emax = 5e3
+    e_photons = np.logspace(np.log10(emin),np.log10(emax),M+1)   
     # frequency vector
     # *1.602e-16 keV ==> J
     f_photons = e_photons * 1.602e-16 / h
@@ -74,7 +70,7 @@ def meshGeneration():
     
     # Valeurs sur les bords des bins (M+1 values)
     #xb = np.logspace(np.log10(xmin),np.log10(xmax),M+1) 
-    xb = np.linspace(xmin,xmax,M+1) 
+    xb = np.logspace(np.log10(xmin),np.log10(xmax),M+1) 
     
     # Largeur des bins (M valeurs)
     dxa = xb[1:] - xb[:-1]
@@ -83,14 +79,10 @@ def meshGeneration():
     # distance entre les milieux (M-1 valeurs)
     dxb = xa[1:] - xa[:-1]
     
-    return xa,xb,dxa,dxb,tobs,tmax,dt,dto
-    
-    
-def solveKompaneets(xa,xb,dxa,dxb,tobs,tmax,dt,dto):
-    """
-    solve the Kompaneets equation with the chang-cooper scheme
-    call on of the plotting function
-    """
+    return xa,xb,dxa,dxb,tobs,dt,dto, M, e_photons, f_photons
+
+
+def initKompParam(xa, f_photons, e_photons, M):
     
     ################# KOMPANEETS EQUATION  PARAMETERS  #################
     
@@ -98,76 +90,111 @@ def solveKompaneets(xa,xb,dxa,dxb,tobs,tmax,dt,dto):
     # the vectors containing the terms from the Kompaneets equation
     # B is defined in the changCooper function as it evolves with the time
     
-    A = xa*xa 
+    # profondeur optique de Thompson
+    pT = Ne * R * sT
+    A = xa**2 * me * cl**2 / pT / k /Te
     C =  0.5*(xa[1:]**4+xa[:-1]**4) # sur les bords      (M-1)
     
-    # injection term
-    Q = np.full_like(xa, 0)            # au milieu          (M)
+    # ---------------- Synchrotron ----------
+        
+    # synchrotron emmisivity
+    p, nuL, Ub, theta, K2 = setParameters(B)
+    # calculus in function of x
+    Jnu = J_nu_theta(f_photons, M, B, p, nuL, Ub, theta, K2)
     
-    # sink term
-    #n*ro = n/T(x) => T(x) = 1/ro
-    ro2=0
-    pech = np.full_like(xa, ro2)            # au milieu          (M)
+    # verif Jnu
+    #plt.plot(e_photons[1:], Jnu)
+    #setFigureParameters(r"Tracé de l'émissivité synchrotron $J_\nu(\nu,\theta)$ pour un gaz d'électrons chauffé à {:} keV".format(kTe),
+    #                    r'$J_\nu(\nu,\theta)$',r'$\nu$',1e-10,1e1,e_photons[1],e_photons[-1])          
     
-    ### initialization of the solution vector : [time]["energy"] (x is a pseudo-energy)
+    Q = Jnu * cl**2 * R / 2 / h / f_photons[1:]**3
     
-    #u=np.empty([N,M])
+    # synchrotron absorption / sink term
+    Bnu = B_nu(f_photons, theta)
+    alpha = Jnu / Bnu
+    # profondeur optique
+    pOpt = alpha*R
+    # pOpt*n + n = n/T(x) => T(x) = 1/(1+pOpt)    
+    # synchrotron absorption AND sink term gathered in T
+    T = 1 / (1 + pOpt)    
     
+    return A, C, Q, T, Jnu, Bnu, pOpt
+    
+
+def showDensities(uobs, xa, dxa):
+    """
+    Compute and print the initial and final densities 
+    of the photons distribution.
+    """
+    Ndens0 = 8*np.pi * ((k*Te)/(cl*h))**3 * np.sum(uobs[0]*dxa*xa**2)
+    print("Densité initiale: ",Ndens0)
+    
+    umax=uobs[len(uobs)-1]
+    Ndens = 8*np.pi * ((k*Te)/(cl*h)) * np.sum(umax*xa**2*dxa)
+    print("Densité finale: ",Ndens)
+    
+    return Ndens0, Ndens
+
+
+def solveKompaneets(xa,xb,dxa,dxb,tobs,dt,dto, M, e_photons, f_photons):
+    """
+    solve the Kompaneets equation with the chang-cooper scheme
+    call of the plotting function
+    """
+    ####### initialization of the parameters of the Kompaneets equation  ######
+    A, C, Q, T, Jnu, Bnu, pOpt = initKompParam(xa, f_photons, e_photons, M)
+
+    ####### initialization of the photons distribution ######
     # photons injection at 0.5 keV, gaussian width of 0.03keV
     einj = 0.5
     ewidth = 0.03
     xinj = einj / kTe
     width = ewidth / kTe
-    print(xinj, width)
-    u0 = init_planckian(e_photons)
-    
-    # Cette fois, on impose le nombre de photons
-    # STABLE EN DECA DE ~2.4
-    #nbPhotons = 1
-    
-    Ndens0 = 8*np.pi * ((k*Te)/(cl*h))**3 * np.sum(u0*dxa*xa**2)
-    print("Densité initiale: ",Ndens0)
-    
-    
+    #u0 = init_planckian(e_photons[1:])
+    u0 = init_gaussian(xb, xinj, width)
+        
     ############  SOLVING KOMPANEETS EQUATION AND PLOTTING FUNCTIONS  ###########
         
     # find the solution of the kompaneets equation with the cc scheme for each instant
     # specified in tobs
-    uobs = np.empty([len(tobs)+1,M])
-    uobs[0] = u0
-    print(len(uobs[0]))
-    for i,tt in enumerate(tobs):    
-        N=int(tmax/dt)
-        uobs[i+1] = cc.changCooper(A, C, xa, dxa, dxb, N, u0, pech, Q, dto)
+    #------------------------------------------------------------------------#
+    uobs = cc.changCooper(A, C, T, Q, xa, dxa, dxb, u0, M, tobs, dt, dto) #
+    #------------------------------------------------------------------------#
+
+    # the initial and final densities of the photons distribution
+    Ndens0, Ndens = showDensities(uobs, xa, dxa)    
+    # theoritical solution of the kompaneets equation WITHOUT the synchrotron emission/absorption
+    #C=findCst(Ndens0)
+    #intensityTh = ((2*h)/cl**2) * f_photons[1:]**3 * ( (1 /  (C*np.exp((h*f_photons[1:])/(k*Te)) - 1 ))) 
     
-    # Calculus of the final density (after 30s)
-    # Must be constant (same as the initial one)
-    umax=uobs[len(uobs)-1]
-    Ndens = 8*np.pi * ((k*Te)/(cl*h)) * np.sum(umax*xa**2*dxa)
-    print("Densité finale: ",Ndens)
+    # synchrotron only
+    N=int(tobs[-1]/dt)
+    uS = cc.changCooper2(xb, N, u0, T, Q, dto)
+    #compton only
+    T= np.zeros_like(xa)
+    Q = np.zeros_like(xa)
+    uobsComp = cc.changCooper(A, C, T, Q, xa, dxa, dxb, u0, M, tobs, dt, dto)
     
-    C=findCst(Ndens0)
-    intensityTh = ((2*h)/cl**2) * f_photons**3 * ( (1 /  (C*np.exp((h*f_photons)/(k*Te)) - 1 ))) 
+    plotIntensityAll(Jnu, Bnu, uS, uobsComp, uobs, tobs, e_photons[1:], f_photons[1:],
+                  'Intensité de la couronne (B = {:.0E} G, kT = {:} keV, R = {:.0E} cm)'.format(Bcgs,kTe,Rcgs),'Spectral Radiance $(J.m^{-2}.s^{-1}.Hz^{-1}.str^{-1})$',
+                  'Energy (keV)',1e-15,5e10,1e-10,1e10)
     
-    plotIntensity(Ndens0, uobs, tobs, e_photons, f_photons)
+    #plotIntensity(0, uobs, tobs, e_photons[1:], f_photons[1:],
+    #             'Intensité de la couronne (B = {:.0E} G, kT = {:} keV, R = {:.0E} cm)'.format(Bcgs,kTe,Rcgs),'Spectral Radiance $(keV.m^{-2}.s^{-1}.Hz^{-1}.str^{-1})$',
+    #             'Energy (keV)',1e-15,5e10,1e-10,1e10)
     
-    phoDensTh = ((8*np.pi)/cl**3) * f_photons**2 / (findCst(Ndens0)*np.exp((h*f_photons)/(k*Te)) - 1 ) 
+    #phoDensFreqTh = ((8*np.pi)/cl**3) * f_photons[1:]**2 / (findCst(Ndens0)*np.exp((h*f_photons[1:])/(k*Te)) - 1 ) 
     #plotDensityFreq(xa, dxa, tobs, dt)
-    
     #phoDensTh = ((8*np.pi)/cl**3) * f_photons**2 * (1 / (findCst(Ndens0)*np.exp((h*f_photons)/(k*Te)) - 1 )) *1.602e-16 / h 
     #plotDensity(xa, dxa, tobs, dt)
-    
     #plotOccupationRate(xa, dxa, tobs, dt)
 
 
 def mainKompaneets():
     
-    xa,xb,dxa,dxb,tobs,tmax,dt,dto = meshGeneration()
+    xa,xb,dxa,dxb,tobs,dt,dto, M, e_photons, f_photons = meshGeneration()
 
-    solveKompaneets(xa,xb,dxa,dxb,tobs,tmax,dt,dto)  
+    solveKompaneets(xa, xb, dxa, dxb, tobs, dt, dto, M, e_photons, f_photons)
     
-    Nmax=findNmax()
-    
-    
-    #print(Nmax)
-    
+
+mainKompaneets()
